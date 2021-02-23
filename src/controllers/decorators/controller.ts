d9698c664face2e63a00e42e15291242ce419894
utkarshchowdhary <utkarshchowdhary4140@gmail.com>
2021-02-23 13:07:33 +0530

    removed redundant check for body existance

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -6,11 +6,6 @@ import { Methods } from "./Methods";
 
 function bodyValidator(keys: string[]): RequestHandler {
   return function (req: Request, res: Response, next: NextFunction) {
-    if (!req.body) {
-      res.status(422).send("Invalid Request!");
-      return;
-    }
-
     for (let key of keys) {
       if (!req.body[key]) {
         res.status(422).send(`Missing Property ${key}`);
